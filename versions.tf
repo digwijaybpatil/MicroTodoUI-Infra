742af5d0cdb8f609f07b940bb4c8eee03a1e7820
digwijaybpatil <digwijaybpatil@gmail.com>
2025-11-26 20:08:33 +0530

    Modified the pipeline

--- versions.tf
+++ versions.tf
@@ -1,12 +1,13 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.54.0"
     }
   }
 }
 
 provider "azurerm" {
-  features{}# Configuration options
+  features {} # Configuration options
+  subscription_id = "09d55635-2a09-4af6-9e93-6bb44383a7aa"
 }
