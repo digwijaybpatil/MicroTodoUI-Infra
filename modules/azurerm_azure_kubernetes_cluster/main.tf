64bc87c0e4cf4e64fc854bbb7e7f090ac2765abe
digwijaybpatil <digwijaybpatil@gmail.com>
2025-11-27 19:12:46 +0530

    Add dns_prefix to Kubernetes cluster resource

--- modules/azurerm_azure_kubernetes_cluster/main.tf
+++ modules/azurerm_azure_kubernetes_cluster/main.tf
@@ -2,12 +2,13 @@ resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.cluster_name
   location            = var.location
   resource_group_name = var.resource_group_name
+  dns_prefix          = "${var.cluster_name}-dns"
 
   default_node_pool {
-    name            = "default"
-    node_count      = 1
-    vm_size         = var.node_vm_size
-    vnet_subnet_id  = var.subnet_id
+    name           = "default"
+    node_count     = 1
+    vm_size        = var.node_vm_size
+    vnet_subnet_id = var.subnet_id
   }
 
   identity {
